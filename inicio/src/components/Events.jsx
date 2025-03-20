b71c0de9d961d3b2de72b62b20b0d423e3c499d1
KLEITON SOBRAL DE OLIVEIRA COSTA <k.costa@labcetec.net>
2025-03-19 21:42:44 -0300

    Eventos e estilização

---
+++ inicio/src/components/Events.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Events = () => {
+    const teste = (e)=> {
+        // e = propriedades do evento que chamou a função
+        console.log(e);
+    };
+  return (
+    <div>
+      <button onClick={() =>{console.log("Testei o primeiro botão")}}>Cli k aqui</button>
+      <button onClick={teste}>Cli k 2 </button>
+    </div>
+  );
+};
+
+export default Events;
---
+++ inicio/src/components/Imagem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Spfc from "../assets"
+
+const Imagem = () => {
+    return (
+        <div>
+            <h1>Imagen</h1>
+            <img src="../"></img>
+        </div>
+    )
+}
+
+export default Image
